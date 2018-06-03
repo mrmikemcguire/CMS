3ecf1ce9b040bf3a4c5a2417177ddfdf47bffb79
Mike McGuire <MoonshotMike@gmail.com>
2018-06-03 12:49:36 -0600

    hook up firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
-import { environment } from '../environments/environment.prod';
+import { environment } from '../environments/environment';
+import { PagesListComponent } from './pages-list/pages-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesListComponent
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages-list/pages-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-pages-list',
+  templateUrl: './pages-list.component.html',
+  styleUrls: ['./pages-list.component.css']
+})
+export class PagesListComponent implements OnInit {
+  pagesObservable: Observable<any[]>;
+  constructor(private db: AngularFireDatabase) { }
+
+  ngOnInit() {
+    this.pagesObservable = this.getPages('/pages');
+  }
+  getPages(listPath): Observable<any[]> {
+    return this.db.list(listPath).valueChanges();
+  }
+}
